51f8b0724fc80c37fb6d3a1dc843eaa91544cf6f
uninstallHahaha <1107185604@qq.com>
2020-12-28 19:32:22 +0800

    Yml2020_12_28#19:32:22

--- init_git_project.go
+++ init_git_project.go
@@ -18,15 +18,15 @@ func InitGitPro(gConfig GitConfig, remoteBranchName *string) {
 	tc1.StopTicker()
 	if gConfig.gitUsername != "" {
 		ExecCommand("git config --global user.name \"" + gConfig.gitUsername + "\"")
-		// fmt.Println(">>>[git config username]")
+		fmt.Println(">>>[git config username]")
 	}
 	if gConfig.gitPassword != "" {
 		ExecCommand("git config --global user.password \"" + gConfig.gitPassword + "\"")
-		// fmt.Println(">>>[git config password]")
+		fmt.Println(">>>[git config password]")
 	}
 	if gConfig.gitUserEmail != "" {
 		ExecCommand("git config --global user.email \"" + gConfig.gitUserEmail + "\"")
-		// fmt.Println(">>>[git config email]")
+		fmt.Println(">>>[git config email]")
 	}
 
 	//init git project
@@ -52,25 +52,25 @@ func InitGitPro(gConfig GitConfig, remoteBranchName *string) {
 	//did not init git project
 	if strings.TrimSpace(res) == "exit status 128" {
 		//init git project
-		// fmt.Println(">>>[git init]")
+		fmt.Println(">>>[git init]")
 		res = ExecCommand("git init")
 		// add remote address
-		// fmt.Println(">>>[git remote add origin " + gConfig.remoteStoreAddress + "]")
+		fmt.Println(">>>[git remote add origin " + gConfig.remoteStoreAddress + "]")
 		res = ExecCommand("git remote add origin " + gConfig.remoteStoreAddress)
 		// add work file to index
-		// fmt.Println(">>>[git add .]")
+		fmt.Println(">>>[git add .]")
 		res = ExecCommand("git add .")
 		// add index to local repository
-		// fmt.Println(">>>[git commit -m " + gConfig.commitPrefix + time.Now().Format("2006_01_02#15:04:05") + "]")
+		fmt.Println(">>>[git commit -m " + gConfig.commitPrefix + time.Now().Format("2006_01_02#15:04:05") + "]")
 		res = ExecCommand("git commit -m " + gConfig.commitPrefix + time.Now().Format("2006_01_02#15:04:05"))
 		// add local branch
-		// fmt.Println(">>>[git branch " + gConfig.localBranchName + "]")
+		fmt.Println(">>>[git branch " + gConfig.localBranchName + "]")
 		res = ExecCommand("git branch " + gConfig.localBranchName)
 		// switch to local branch
-		// fmt.Println(">>>[git checkout " + gConfig.localBranchName + "]")
+		fmt.Println(">>>[git checkout " + gConfig.localBranchName + "]")
 		res = ExecCommand("git checkout " + gConfig.localBranchName)
 		// get remote branchs
-		// fmt.Println(">>>[git fetch]")
+		fmt.Println(">>>[git fetch]")
 		res = ExecCommand("git fetch")
 		// update binding remote branch
 		if gConfig.forceRecover == "0" {
@@ -87,10 +87,10 @@ func InitGitPro(gConfig GitConfig, remoteBranchName *string) {
 			}
 		}
 		// push local repository to remote
-		// fmt.Println(">>>[git push -u origin " + gConfig.localBranchName + ":" + *remoteBranchName + " --force]")
+		fmt.Println(">>>[git push -u origin " + gConfig.localBranchName + ":" + *remoteBranchName + " --force]")
 		res = ExecCommand("git push -u origin " + gConfig.localBranchName + ":" + *remoteBranchName + " --force")
 		// bind local branch with remote branch
-		// fmt.Println(">>>[git branch --set-upstream-to=origin/" + *remoteBranchName + " " + gConfig.localBranchName + "]")
+		fmt.Println(">>>[git branch --set-upstream-to=origin/" + *remoteBranchName + " " + gConfig.localBranchName + "]")
 		res = ExecCommand("git branch --set-upstream-to=origin/" + *remoteBranchName + " " + gConfig.localBranchName)
 
 		fmt.Println("Initing git project is done, now your local files is connecting with your git store")
