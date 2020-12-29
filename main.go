3e0a5247476673d4882d9ca8cf330a1ad249bf1d
uninstallHahaha <1107185604@qq.com>
2020-12-29 10:27:30 +0800

    Yml2020_12_29#10:27:28

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 	_ "github.com/uninstallHahaha/deamon"
 )
 
+// go build -ldflags “-H=windowsgui”
+
 //git config
 var gConfig = GitConfig{}
 
@@ -58,45 +60,45 @@ func startMonitor() {
 	done := make(chan bool)
 
 	go func() {
-	//task key
-	var taskKey int
-	for {
-		select {
-		case event := <-watcher.Events:
-			//exclude .git
-			if event.Name == ".git" {
-				continue
-			}
-			//file change, add and commit
-			fmt.Printf("Probe file change-> Path: %#v, code: %#v\n", event.Name, event.Op)
-			//when add a new folder, add to watcher
-			if event.Op == 0x1 {
-				if !IsFile(event.Name) {
-					watcher.Add(event.Name)
+		//task key
+		var taskKey int
+		for {
+			select {
+			case event := <-watcher.Events:
+				//exclude .git
+				if event.Name == ".git" {
+					continue
 				}
-			}
-			var timer = time.NewTimer(time.Second * time.Duration(gConfig.saveDuration))
-			rand.Seed(time.Now().UnixNano())
-			taskKey = rand.Int() //本次任务的key
-			go func(key int) {
-				<-timer.C
-				if key == taskKey {
-					ExecCommand("git checkout " + gConfig.localBranchName)
-					fmt.Println(">>>[git add .]")
-					ExecCommand("git add .")
-					fmt.Println(">>>[git commit -m " + gConfig.commitPrefix + time.Now().Format("2006_01_02#15:04:05") + "]")
-					ExecCommand("git commit -m " + gConfig.commitPrefix + time.Now().Format("2006_01_02#15:04:05"))
-					fmt.Println(">>>[git push]")
-					ExecCommand("git push --force")
-					taskKey = 0
-					fmt.Println(">>>[push success]")
-					fmt.Println("Waiting for your next edit...")
+				//file change, add and commit
+				fmt.Printf("Probe file change-> Path: %#v, code: %#v\n", event.Name, event.Op)
+				//when add a new folder, add to watcher
+				if event.Op == 0x1 {
+					if !IsFile(event.Name) {
+						watcher.Add(event.Name)
+					}
 				}
-			}(taskKey)
-		case err := <-watcher.Errors:
-			fmt.Println("monitor event error", err)
+				var timer = time.NewTimer(time.Second * time.Duration(gConfig.saveDuration))
+				rand.Seed(time.Now().UnixNano())
+				taskKey = rand.Int() //本次任务的key
+				go func(key int) {
+					<-timer.C
+					if key == taskKey {
+						ExecCommand("git checkout " + gConfig.localBranchName)
+						fmt.Println(">>>[git add .]")
+						ExecCommand("git add .")
+						fmt.Println(">>>[git commit -m " + gConfig.commitPrefix + time.Now().Format("2006_01_02#15:04:05") + "]")
+						ExecCommand("git commit -m " + gConfig.commitPrefix + time.Now().Format("2006_01_02#15:04:05"))
+						fmt.Println(">>>[git push]")
+						ExecCommand("git push --force")
+						taskKey = 0
+						fmt.Println(">>>[push success]")
+						fmt.Println("Waiting for your next edit...")
+					}
+				}(taskKey)
+			case err := <-watcher.Errors:
+				fmt.Println("monitor event error", err)
+			}
 		}
-	}
 	}()
 
 	<-done
